4ce86941a7777aa74773cc231f849720f4041a1a
Hakim408 <128591813+Hakim408@users.noreply.github.com>
2023-03-26 18:01:42 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const getColor = () => {
+    // Hex Code
+    const randomNumber = Math.floor(Math.random() * 16777215);
+    const randomCode = "#" + randomNumber.toString(16);
+    document.body.style.backgroundColor = randomCode;
+
+}
+
+//event call
+document.getElementById("btn").addEventListener(
+    "click",
+    getColor
+)
+
+
+// init call
+getColor();
